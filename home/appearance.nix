ccf41c1fd0b4fbb7e09aa986622a99ee88d1f1c0
Connor du Toit <connordutoit@gmail.com>
2026-08-25 21:43:13 +0200

    NixOS generation 104 (26.11.20260819.ffb3c9b)

--- home/appearance.nix
+++ home/appearance.nix
@@ -7,26 +7,26 @@
     package = pkgs.adwaita-icon-theme;
   };
 
-  # ordering the file pickers / explorers all the same
-  dconf.enable = true;
-  dconf.settings = {
-    "org/gnome/desktop/interface".icon-theme = "Adwaita";
+  # # ordering the file pickers / explorers all the same
+  # dconf.enable = true;
+  # dconf.settings = {
+  #   "org/gnome/desktop/interface".icon-theme = "Adwaita";
 
-    "org/gnome/nautilus/preferences" = {
-      default-sort-order = "name";
-      default-sort-in-reverse-order = false;
-    };
+  #   "org/gnome/nautilus/preferences" = {
+  #     default-sort-order = "name";
+  #     default-sort-in-reverse-order = false;
+  #   };
 
-    "org/gtk/gtk4/settings/file-chooser" = {
-      sort-column = "name";
-      sort-order = "ascending";
-      sort-directories-first = true;
-    };
+  #   "org/gtk/gtk4/settings/file-chooser" = {
+  #     sort-column = "name";
+  #     sort-order = "ascending";
+  #     sort-directories-first = true;
+  #   };
 
-    "org/gtk/settings/file-chooser" = {
-      sort-column = "name";
-      sort-order = "ascending";
-      sort-directories-first = true;
-    };
-  };
+  #   "org/gtk/settings/file-chooser" = {
+  #     sort-column = "name";
+  #     sort-order = "ascending";
+  #     sort-directories-first = true;
+  #   };
+  # };
 }
